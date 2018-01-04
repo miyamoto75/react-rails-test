c0921d814eb8d9ec4b2f502049e78bbf76fe2a9c
miyamoto yuji <mymt.yuji0612@gmail.com>
2018-01-04 22:25:01 +0900

    get client from API on angular

--- public/app.js
+++ public/app.js
@@ -13,7 +13,7 @@ app.controller('MainController', ['$scope', function ($scope) {
 
 }]);
 
-app.controller('ClientController', ['$scope', function ($scope) {
+app.controller('ClientController', ['$scope', '$http', function ($scope, $http) {
   // $scope.clients = [
   //   {id: 1, name: "a", pref: "H"},
   //   {id: 2, name: "b", pref: "O"},
@@ -21,8 +21,20 @@ app.controller('ClientController', ['$scope', function ($scope) {
   // ];
 
   $scope.getClient = function () {
-    $scope.clients.push({id: 999, name: "x", pref: "S"});
+    // $scope.clients.push({id: 999, name: "x", pref: "S"});
     // console.log($scope.clients);
+    $http({
+      method: 'GET',
+      url: "http://localhost:3000/api/v1/clients/get",
+    }).then( function (data) {
+      var c = data.data;
+      c.pref = "ajax";
+      // console.log(c);
+      $scope.clients.push(c);
+    }, function (err) {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+    });
   };
 
 }]);
