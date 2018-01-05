11b7a88c691ad44f7c81a4562b91c147868c3ef5
miyamoto yuji <mymt.yuji0612@gmail.com>
2018-01-05 13:56:03 +0900

    API add pref

--- app/controllers/api/v1/clients_controller.rb
+++ app/controllers/api/v1/clients_controller.rb
@@ -2,7 +2,8 @@ class Api::V1::ClientsController < ApplicationController
   def get
     c = {
       id: rand(100..9999999),
-      name: (3...20).map{ (65 + rand(26)).chr }.join
+      name: (3...20).map{ (65 + rand(26)).chr }.join,
+      pref: (65 + rand(26)).chr,
     }
 
     render plain: c.to_json
