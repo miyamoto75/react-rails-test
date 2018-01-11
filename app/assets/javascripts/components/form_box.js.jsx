94b4b7bca8e8583d7edd859916a987f117e3821a
miyamoto@rensa <yuji.miyamoto@rensa.co.jp>
2018-01-11 20:44:23 +0900

    こっそりフォーム追加、自動で大文字入力

--- app/assets/javascripts/components/form_box.js.jsx
+++ app/assets/javascripts/components/form_box.js.jsx
@@ -4,11 +4,33 @@ var FormBox = createReactClass({
     return (
       <div className="row">
         <div className="col-md-6 col-md-offset-1">
-
-          <p>geho</p>
+          <FormTextInput />
         </div>
       </div>
     );
   }
 });
+
+
+var FormTextInput = createReactClass({
+  getInitialState: function() {
+    return {
+      helloTo: 'input text!!'
+    };
+  },
+
+  changeHandle: function(e) {
+    this.setState({helloTo: e.target.value.toUpperCase()});
+  },
+
+  render: function() {
+    return (
+      <div className="row">
+        <h3>&nbsp;{this.state.helloTo}</h3>
+        <input type="text" onChange={this.changeHandle} value={this.state.helloTo} />
+      </div>
+    );
+  }
+});
+
 
